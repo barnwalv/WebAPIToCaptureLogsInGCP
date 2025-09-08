a3b050fd8c07563c98da78b690091484f902f65b
vbarnwal <vivekbarnwal85@gmail.com>
2025-09-08 14:35:34 +0530

    capture live application logs to gcp log explorer

--- WebAPIToCaptureLogsInGCP/Program.cs
+++ WebAPIToCaptureLogsInGCP/Program.cs
@@ -1,11 +1,33 @@
+using Google.Api;
+using Google.Cloud.Logging.V2;
+using GoogleLogs4DotNet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Set the environment variable for Google Cloud credentials
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\vkbar\Downloads\service-account.json");
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<LoggingServiceV2Client>(provider => LoggingServiceV2Client.Create());
+
+
+builder.Services.AddSingleton<GoogleLogger>(sp =>
+{
+    var projectId = builder.Configuration["GoogleCloud:ProjectId"];
+    var uniqueLogId = builder.Configuration["GoogleCloud:LogId"];
+    return new GoogleLogger(projectId, uniqueLogId);
+});
+
+
 
 var app = builder.Build();
 
